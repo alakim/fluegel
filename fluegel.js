307c452179041a653e9afb65e0fc4dd16d229aa9
alakim <alakm@rambler.ru>
2017-09-04 15:15:55 +0300

    Doctype definition and localized tag labels

--- fluegel.js
+++ fluegel.js
@@ -58,198 +58,63 @@
 		};
 	})();
 
-	function selectWith(tagName){
-		var selection = window.getSelection(),
-			bgn = selection.getRangeAt(0),
-			end = selection.getRangeAt(selection.rangeCount-1);
-		// console.log(selection, bgn, end);
-		// console.log(bgn.startContainer.nodeValue, bgn.startOffset, ' to ', end.endContainer.nodeValue, end.endOffset);
 
+	function init(editorPnl, config, docText){
+		var templates = {
+			marker: function(name, closing){
+				var sz = Settings.marker.size;
+				return $C.html.canvas({
+					'class':'marker',
+					width:px(sz.w), height: px(sz.h),
+					'data-name':name,
+					'data-closing':!!closing
+				});
+			},
+			markerDraw: function(el, highlight){
+				var sz = Settings.marker.size;
+				var name = $(el).attr('data-name'),
+					closing = $(el).attr('data-closing')=='true',
+					unclosed = $(el).attr('data-unclosed')=='true';
+				var ctx = el.getContext('2d');
+				var label = name;
+				
+				var def = el.tagDef || editorPnl.tagsByName[name];
+				if(def&&def.label) label = def.label.text;
 
-		function insertTag(node, pos, name, closing){
-			var txt = node.nodeValue,
-				txtBefore = txt.slice(0, pos),
-				txtAfter = txt.slice(pos, txt.length);
-
-			node.parentNode.insertBefore(document.createTextNode(txtBefore), node);
-			insertMarker(node, name, closing)
-			node.parentNode.insertBefore(document.createTextNode(txtAfter), node);
-			node.parentNode.removeChild(node);
-		}
-
-		function insertTagsPair(node, pos1, pos2, name){
-			var txt = node.nodeValue,
-				t1 = txt.slice(0, pos1),
-				t2 = txt.slice(pos1, pos2),
-				t3 = txt.slice(pos2, txt.length);
-			//console.log(t1, t2, t3);
-			node.parentNode.insertBefore(document.createTextNode(t1), node);
-			insertMarker(node, name)
-			node.parentNode.insertBefore(document.createTextNode(t2), node);
-			insertMarker(node, name, true)
-			node.parentNode.insertBefore(document.createTextNode(t3), node);
-			node.parentNode.removeChild(node);
-		}
-
-		function insertMarker(node, name, closing){
-			var cnv = document.createElement('canvas');
-			var sz = Settings.marker.size;
-			cnv.setAttribute('class', 'marker');
-			cnv.setAttribute('width', px(sz.w));
-			cnv.setAttribute('height', px(sz.h));
-			cnv.setAttribute('data-name', name);
-			cnv.setAttribute('data-closing', !!closing);
-			node.parentNode.insertBefore(cnv, node);
-			templates.markerDraw(cnv, false);
-			$(cnv).mouseover(function(){
-				highlightOpposite($(this));
-			}).mouseout(function(){
-				highlightOpposite($(this), true);
-			});
-		}
-
-
-		if(bgn.startContainer==bgn.endContainer){
-			insertTagsPair(bgn.startContainer, bgn.startOffset, bgn.endOffset, tagName);
-		}
-		else{
-			insertTag(bgn.startContainer, bgn.startOffset, tagName, false);
-			insertTag(end.endContainer, end.endOffset, tagName, true);
-		}
-
-		if (window.getSelection) {
-			if (window.getSelection().empty) {  // Chrome
-				window.getSelection().empty();
-			} else if (window.getSelection().removeAllRanges) {  // Firefox
-				window.getSelection().removeAllRanges();
-			}
-		} else if (document.selection) {  // IE?
-			document.selection.empty();
-		}
-
-		setOpposites($('.fluegel'));
-	}
-
-	var templates = {
-		marker: function(name, closing){
-			var sz = Settings.marker.size;
-			return $C.html.canvas({
-				'class':'marker',
-				width:px(sz.w), height: px(sz.h),
-				'data-name':name,
-				'data-closing':!!closing
-			});
-		},
-		markerDraw: function(el, highlight){
-			var sz = Settings.marker.size;
-			var name = $(el).attr('data-name'),
-				closing = $(el).attr('data-closing')=='true',
-				unclosed = $(el).attr('data-unclosed')=='true';
-			var ctx = el.getContext('2d');
-			ctx.clearRect(0, 0, sz.w, sz.h);
-			ctx.fillStyle = 
-				unclosed?Settings.marker.bgColor.error
-					:highlight?Settings.marker.bgColor.hi
-					:Settings.marker.bgColor.lo;
-			ctx.beginPath();
-			if(closing){
-				ctx.moveTo(sz.w/2, 0);
-				ctx.lineTo(sz.w, 0);
-				ctx.lineTo(sz.w, sz.h);
-				ctx.lineTo(sz.w/2, sz.h);
-				ctx.lineTo(0, sz.h/2);
-			}
-			else{
-				ctx.moveTo(0, 0);
-				ctx.lineTo(sz.w/2, 0);
-				ctx.lineTo(sz.w, sz.h/2);
-				ctx.lineTo(sz.w/2, sz.h);
-				ctx.lineTo(0, sz.h);
-			}
-			ctx.fill();
-
-			ctx.fillStyle = highlight?Settings.marker.color.hi
-					:Settings.marker.color.lo;
-			ctx.font = '12px Arial';
-			ctx.fillText(name, closing?sz.w/2:sz.w*.2, sz.h*.8);
-			
-		}
-	};
-
-	function insertMarkers(docText){
-		return docText.replace(/<(\/)?([^>]+)>/gi, function(str, closing, name){
-			return templates.marker(name, closing);
-		});
-	}
-
-	function highlightOpposite(marker, hide){
-		templates.markerDraw(marker[0], !hide);
-		templates.markerDraw(marker[0].opposite, !hide);
-		
-	}
-
-
-	function getStyle(str){
-		var res = {};
-		var pairs = str.split(';');
-		for(var p,i=0; p=pairs[i],i<pairs.length; i++){
-			var v = p.split(':');
-			if(v[0].length){
-				res[v[0]] = v[1];
-			}
-		}
-		return res;
-	}
-	function setStyle(el, style){
-		var res = [];
-		for(var k in style){
-			res.push([k, style[k]].join(':'));
-		}
-		$(el).attr({style: res.join(';')});
-	}
-
-	function setOpposites(el){
-		var path = [];
-		el.find('.marker').each(function(i, mrk){mrk=$(mrk);
-			var closed = mrk.attr('data-closed')=='true';
-			if(closed) return;
-
-			var nm = mrk.attr('data-name'),
-				closing = mrk.attr('data-closing')=='true';
-
-			if(closing){
-				var opened = path[path.length-1],
-					oNm = opened.attr('data-name');
-				if(oNm!=nm){
-					console.error('Unclosed tag %s', oNm);
-					mrk.attr('data-unclosed', true);
-					return;
+				ctx.clearRect(0, 0, sz.w, sz.h);
+				ctx.fillStyle = 
+					unclosed?Settings.marker.bgColor.error
+						:highlight?Settings.marker.bgColor.hi
+						:Settings.marker.bgColor.lo;
+				ctx.beginPath();
+				if(closing){
+					ctx.moveTo(sz.w/2, 0);
+					ctx.lineTo(sz.w, 0);
+					ctx.lineTo(sz.w, sz.h);
+					ctx.lineTo(sz.w/2, sz.h);
+					ctx.lineTo(0, sz.h/2);
 				}
-				opened[0].opposite = mrk[0];
-				mrk[0].opposite = opened[0];
-				path.splice(path.length-1, 1);
-				mrk.attr('data-unclosed', false);
-			}
-			else{
-				path.push(mrk);
-			}
-		});
-	}
+				else{
+					ctx.moveTo(0, 0);
+					ctx.lineTo(sz.w/2, 0);
+					ctx.lineTo(sz.w, sz.h/2);
+					ctx.lineTo(sz.w/2, sz.h);
+					ctx.lineTo(0, sz.h);
+				}
+				ctx.fill();
 
-	function drawMarkers(pnl){
-		$(pnl).find('.marker').each(function(i, el){
-			templates.markerDraw(el);
-		});
-	}
+				ctx.fillStyle = highlight?Settings.marker.color.hi
+						:Settings.marker.color.lo;
+				ctx.font = '12px Arial';
+				ctx.fillText(label, closing?sz.w/2:sz.w*.2, sz.h*.8);
+				
+			}
+		};
 
-	function init(el, config, docText){
-		el.addClass('fluegel')
+		editorPnl.addClass('fluegel')
 			.html((function(){with($H){
 				return markup(
-					div({'class':'fluegelButtonsPanel'},
-						button({'class':'btSelI'}, 'I'),
-						button({'class':'btSelB'}, 'B')
-					),
+					div({'class':'fluegelButtonsPanel'}),
 					div({'class':'fluegelEditor', contenteditable:true})
 				);
 			}})())
@@ -274,23 +139,172 @@
 				ev.preventDefault();
 				var sel = getCodeSelection();
 
-				// console.log('Clipboard: %s', clipBoard);
-
 				setText(sel.textBefore+clipBoard+sel.textAfter);
 
 			})
-			.find('.btSelI').click(function(){
-				selectWith('i');
-			}).end()
-			.find('.btSelB').click(function(){
-				selectWith('b');
-			}).end()
 		;
+
+		function insertMarkers(docText){
+			return docText.replace(/<(\/)?([^>]+)>/gi, function(str, closing, name){
+				return templates.marker(name, closing);
+			});
+		}
+
+		function drawButtons(){
+			if(!config.doctype) return;
+			editorPnl.tagDefinitions = [];
+			editorPnl.tagsByName = {};
+			for(var t,i=0; t=config.doctype[i],i<config.doctype.length; i++){
+				t.id = editorPnl.tagDefinitions.length;
+				editorPnl.tagDefinitions.push(t);
+				editorPnl.tagsByName[t.tag] = t;
+			}
+
+			$(editorPnl).find('.fluegelButtonsPanel')
+				.html((function(){with($H){
+					return apply(config.doctype, function(t){
+						return button({'class':'tagButton', 
+								'data-tagID':t.id
+							},
+							t.description?{title:t.description}:null,
+							t.label?
+								t.label.style?{style:t.label.style}:null
+								:null,
+							t.label && t.label.text?t.label.text:t.tag
+						);
+					});
+				}})())
+				.find('.tagButton').click(function(){
+					selectWith(
+						editorPnl.tagDefinitions[
+							parseInt($(this).attr('data-tagID'))
+						]
+					);
+				}).end()
+			;
+		}
+		drawButtons();
+
+		function drawMarkers(){
+			$(editorPnl).find('.marker').each(function(i, el){
+				templates.markerDraw(el);
+			});
+		}
+
+		function selectWith(def){
+			var tagName = def.tag,
+				selfClosing = def.selfClosing;
+
+			var selection = window.getSelection();
+			if(!selection.rangeCount) return;
+			var bgn = selection.getRangeAt(0),
+				end = selection.getRangeAt(selection.rangeCount-1);
+
+			function insertTag(node, pos, name, closing){
+				var txt = node.nodeValue,
+					txtBefore = txt.slice(0, pos),
+					txtAfter = txt.slice(pos, txt.length);
+
+				node.parentNode.insertBefore(document.createTextNode(txtBefore), node);
+				insertMarker(node, name, closing)
+				node.parentNode.insertBefore(document.createTextNode(txtAfter), node);
+				node.parentNode.removeChild(node);
+			}
+
+			function insertTagsPair(node, pos1, pos2, name){
+				var txt = node.nodeValue;
+				if(!txt) return;
+				var t1 = txt.slice(0, pos1),
+					t2 = txt.slice(pos1, pos2),
+					t3 = txt.slice(pos2, txt.length);
+				//console.log(t1, t2, t3);
+				node.parentNode.insertBefore(document.createTextNode(t1), node);
+				insertMarker(node, name)
+				node.parentNode.insertBefore(document.createTextNode(t2), node);
+				insertMarker(node, name, true)
+				node.parentNode.insertBefore(document.createTextNode(t3), node);
+				node.parentNode.removeChild(node);
+			}
+
+			function insertMarker(node, name, closing){
+				var cnv = document.createElement('canvas');
+				var sz = Settings.marker.size;
+				cnv.setAttribute('class', 'marker');
+				cnv.setAttribute('width', px(sz.w));
+				cnv.setAttribute('height', px(sz.h));
+				cnv.setAttribute('data-name', name);
+				cnv.setAttribute('data-closing', !!closing);
+				cnv.tagDef = def;
+				node.parentNode.insertBefore(cnv, node);
+				templates.markerDraw(cnv, false);
+				$(cnv).mouseover(function(){
+					highlightOpposite($(this));
+				}).mouseout(function(){
+					highlightOpposite($(this), true);
+				});
+			}
+
+
+			if(bgn.startContainer==bgn.endContainer){
+				insertTagsPair(bgn.startContainer, bgn.startOffset, bgn.endOffset, tagName);
+			}
+			else{
+				insertTag(bgn.startContainer, bgn.startOffset, tagName, false);
+				insertTag(end.endContainer, end.endOffset, tagName, true);
+			}
+
+			if (window.getSelection) {
+				if (window.getSelection().empty) {  // Chrome
+					window.getSelection().empty();
+				} else if (window.getSelection().removeAllRanges) {  // Firefox
+					window.getSelection().removeAllRanges();
+				}
+			} else if (document.selection) {  // IE?
+				document.selection.empty();
+			}
+
+			setOpposites();
+		}
+
+		function highlightOpposite(marker, hide){
+			templates.markerDraw(marker[0], !hide);
+			templates.markerDraw(marker[0].opposite, !hide);
+			
+		}
+
+		function setOpposites(){
+			var path = [];
+			editorPnl.find('.marker').each(function(i, mrk){mrk=$(mrk);
+				var closed = mrk.attr('data-closed')=='true';
+				if(closed) return;
+
+				var nm = mrk.attr('data-name'),
+					closing = mrk.attr('data-closing')=='true';
+
+				if(closing){
+					var opened = path[path.length-1],
+						oNm = opened.attr('data-name');
+					if(oNm!=nm){
+						console.error('Unclosed tag %s', oNm);
+						mrk.attr('data-unclosed', true);
+						return;
+					}
+					opened[0].opposite = mrk[0];
+					mrk[0].opposite = opened[0];
+					path.splice(path.length-1, 1);
+					mrk.attr('data-unclosed', false);
+				}
+				else{
+					path.push(mrk);
+				}
+			});
+		}
+
 
 		setText(docText);
 
 		function setText(docText){
-			el.find('.fluegelEditor')
+			editorPnl.find('.fluegelEditor')
 				.html(insertMarkers(docText))
 				.find('.marker').mouseover(function(){
 					highlightOpposite($(this));
@@ -299,9 +313,9 @@
 				}).end()
 			;
 
-			drawMarkers(el);
+			drawMarkers();
 
-			setOpposites(el);
+			setOpposites();
 		}
 
 		function getCodeSelection(){
@@ -341,7 +355,7 @@
 		}
 
 		function harvest(node, insertIDs){
-			node = node || el.find('.fluegelEditor')[0];
+			node = node || editorPnl.find('.fluegelEditor')[0];
 			switch(node.nodeName){
 				case 'CANVAS':
 					var nd = $(node),
